9b4f0ea8f4aa08360bc5a0336d3cff1600828626
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-03-30 11:13:08 +0530

    feat: initialize minwind project repository and add utility scripts for version comparison and documentation

---
+++ 05-DSA-Masterji-js/src/convertBase7.ts
@@ -0,0 +1,9 @@
+function convertToBase(num: number): string {
+    // num: integer to be converted to base 7
+
+
+    return num.toString(7);
+}
+
+console.log(convertToBase(100));
+console.log(convertToBase(-7));
