93811af0b54a62a32e5726dfb9f7bc756e9053c5
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-06 13:51:25 +0530

    clean up: Backend update

--- 01-Weekend-Classes/05-Backend Engineering/01-CodeFiles/03-nodejs_internals/nodejs-internals/1-one.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-setTimeout(() => console.log('Hello from Timer'), 0);
-
-console.log('Hello from Top Level Code');
--- 01-Weekend-Classes/05-Backend Engineering/01-CodeFiles/03-nodejs_internals/nodejs-internals/2-two.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-setTimeout(() => console.log('Hello from Timer'), 0);
-setImmediate(() => console.log('Hello from Immediate'), 0);
-
-console.log('Hello from Top Level Code');
