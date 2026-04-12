42085179e9e889f5710894a661558fbf0c25ccc0
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-13 02:02:01 +0530

    feat: implement authentication module with registration controller, routes, service, and validation DTO

--- 01-Weekend-Classes/05-Backend Engineering/03-API/01-Build REST-API/src/modules/auth/auth.controller.js
+++ 01-Weekend-Classes/05-Backend Engineering/03-API/01-Build REST-API/src/modules/auth/auth.controller.js
@@ -1,7 +1,10 @@
+import * as authService from "./auth.service.js"
+import ApiResponse from "../../common/utils/api-response.js"
 
-const register = async () => {
+const register = async (req, res) => {
     // something
-    
+    const user = authService.register(req.body)
+    ApiResponse.created(res, "Registration success", user)
 }
 
 export {register};
---
+++ 01-Weekend-Classes/05-Backend Engineering/03-API/01-Build REST-API/src/modules/auth/auth.service.js
@@ -0,0 +1,9 @@
+const register = async ({ name, email, password, role }) => {
+    // do user registeration
+
+    let userObj;
+
+    return userObj;
+}
+
+export { register };
