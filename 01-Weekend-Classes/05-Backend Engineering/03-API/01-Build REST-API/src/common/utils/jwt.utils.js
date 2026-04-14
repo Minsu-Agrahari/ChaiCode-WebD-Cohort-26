a76c13a9c7b4b5c2b0468aff6c0af67ad7a6ddc8
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-14 20:05:50 +0530

    auth token creation

--- 01-Weekend-Classes/05-Backend Engineering/03-API/01-Build REST-API/src/common/utils/jwt.utils.js
+++ 01-Weekend-Classes/05-Backend Engineering/03-API/01-Build REST-API/src/common/utils/jwt.utils.js
@@ -1,17 +1,42 @@
 // Token -> Long String
 
 import crypto from "crypto"
+import jwt from "jsonwebtoken";
 
-const generateResetToken = () => {
-    const rawToken = crypto.randomBytes(32)/toString("hex")
-    const hashedToken = crypto
-                        .createHash("sha256")
-                        .update(rawToken)
-                        .digest("hex")
+const generateAccessToken = (payload) => {
+    return jwt.sign.apply(payload, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
+    });
+};
 
-    return {rawToken, hashedToken}
+const verifyAccessToken = (token) => {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
 }
 
-export{
-    generateResetToken
+const generateRefershToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_REFERSH_SECRET, {
+        expiresIn: process.env.JWT_REFERSH_EXPIRES_IN || "7d",
+    });
+};
+
+const verifyRefreshToken = (token) => {
+    return jwt.verify(token, process.env.JWT_REFERSH_SECRET);
+}
+
+const generateResetToken = () => {
+    const rawToken = crypto.randomBytes(32) / toString("hex")
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(rawToken)
+        .digest("hex")
+
+    return { rawToken, hashedToken }
+}
+
+export {
+    generateResetToken,
+    verifyAccessToken,
+    verifyRefreshToken,
+    generateAccessToken,
+    generateRefershToken,
 }
