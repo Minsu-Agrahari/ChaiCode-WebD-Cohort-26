a4ebe6a91d85c5fcd63051bb8a6ba022f02b02b1
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-20 00:35:10 +0530

    file name changed

--- 01-Weekend-Classes/05-Backend Engineering/03-API/2.Auth-TS/drizzle.config.js
+++ 01-Weekend-Classes/05-Backend Engineering/03-API/2.Auth-TS/drizzle.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
     schema: './src/db/Schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL,
     },
 });
