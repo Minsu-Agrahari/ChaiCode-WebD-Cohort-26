8f58b380d3209cf706073b1909b7cf1828b24571
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-25 05:24:08 +0530

    sign-in route done

--- 01-Weekend-Classes/05-Backend Engineering/03-API/2.Auth-TS/src/app/auth/controller.ts
+++ 01-Weekend-Classes/05-Backend Engineering/03-API/2.Auth-TS/src/app/auth/controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
 import { createHmac, randomBytes } from 'node:crypto';
-import { signupPayloadModel } from './models'
+import { signinPayloadModel, signupPayloadModel } from './models'
 import { db } from '../../db';
 import { userTable } from '../../db/schema';
 import { eq } from 'drizzle-orm';
 
 class AuthenticationController {
-    public async handleSignup(req: Request, res: Response){
+    public async handleSignup (req: Request, res: Response){
 
         const validationResult = await signupPayloadModel.safeParseAsync(req.body);
         
@@ -31,6 +31,27 @@ class AuthenticationController {
         
         return res.status(201).json({message: 'user has been created successfully', data: {id: result?.id}})
     }
+
+    // TODO: put DTO layer || DRY prinicple violents
+    public async handleSignin (req: Request, res: Response){
+        const validationResult = await signinPayloadModel.safeParseAsync(req.body)
+
+        if(validationResult.error) return res.status(400).json({message: 'body validation failed', error: (await validationResult).error.issues})
+            
+        const {email, password} = validationResult.data
+
+        const [userSelect] = await db.select().from(userTable).where(eq(userTable.email, email))
+        if(!userSelect) return res.status(404).json({message: `user with email ${email} does not exists`})
+    
+        const salt = userSelect.salt!
+        const hash = createHmac('sha256', salt).update(password).digest('hex')
+
+        if(userSelect.password !== hash) return res.status(400).json({message: `email or password is incorrect`})
+    
+        // TODO: Token Creation
+        return res.json({message: 'Signin Success', data: {token: 1}})
+    }
+
 }
 
 export default AuthenticationController
