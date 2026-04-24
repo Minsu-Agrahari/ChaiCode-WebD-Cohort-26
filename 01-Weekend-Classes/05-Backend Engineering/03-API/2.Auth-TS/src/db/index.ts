fc9c18708dda824758158b4635bb79725bc7d18a
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-04-24 15:35:07 +0530

    [new update] rewrite the code base

--- 01-Weekend-Classes/05-Backend Engineering/03-API/2.Auth-TS/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-// line 1-6: Connect Drizzle ORM to the database -- <Drizzle website>
-
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-export const db = drizzle(process.env.DATABASE_URL!);
-
